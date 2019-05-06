e74ed3eaf4b26e6a73c6b33d9093b2269d9a331e
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-06 13:10:38 +0100

    Feat: Retrieve information about the last car in the inventory

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,7 +74,6 @@ let inventory = [
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(inventory[1].car_make);
 for (let i = 0; i < inventory.length; i++) {
     if(inventory[i].id === 33) {
         console.log(
@@ -87,7 +86,14 @@ for (let i = 0; i < inventory.length; i++) {
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+    if(inventory[i].id === inventory.length) {
+        console.log(
+        `Car ${inventory.length} is a ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`
+        );
+    }
+}
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
